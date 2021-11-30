0c6a50541bd390126975e00aa9da64c3da488869
aubriebarnhart <aubrieb@live.unc.edu>
2021-11-30 17:50:16 -0500

    added first data points in form.js

---
+++ form.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="node_modules/bulma/css/bulma.css">
+    <link rel="stylesheet" href="custom_style.css">
+    <title>Assignment 5</title>
+</head>
